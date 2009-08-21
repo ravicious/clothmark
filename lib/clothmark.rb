45af7d6a340f3687cda45d60014299033a9fab3a
Rafal Cieslak <ravicious@gmail.com>
2009-08-21 14:29:20 +0200

    Code refactoring

--- lib/clothmark.rb
+++ lib/clothmark.rb
@@ -3,5 +3,100 @@ require "BlueCloth"
 require "RedCloth"
 require "bb-ruby"
 
-require File.dirname(__FILE__) + '/clothmark/module'
-require File.dirname(__FILE__) + '/clothmark/markups'
+class ClothMark
+  attr_accessor :data_for_output, :file, :output
+  attr_reader :markup
+ 
+  # ClothMark will generate a default filename if user don't want to save output to a specific file
+  # and will use Markdown as a default markup language.
+  def initialize(file, markup = "markdown", additional_html = false, output = nil)
+    @file = file
+
+    # ClothMark will raise an error if unknown markup language has been sent
+    if %w(markdown textile bbcode).include? markup
+      @markup = markup
+    else
+      raise ArgumentError, "Unknown markup language (#{markup})"
+    end
+
+    if (!output || output.empty?)
+      @output = "#{file.gsub(/(\.[a-z]{3,4})/, '')}_clothmark.html"
+    else
+      @output = output
+    end
+    @data_for_output = []
+    @additional_html = additional_html # If true, then ClothMark will generate additional CSS and HTML
+  end
+  
+  # Reads data from input file and puts it to array
+  def read_from_file
+    begin
+    File.open(@file) do |file|
+      file.each_line {|line| @data_for_output << line}
+    end
+    rescue Errno::ENOENT => e
+      puts e
+      exit
+    end
+  end
+
+  def to_html
+    case @markup
+    when 'markdown'
+      @data_for_output.collect! {|line| BlueCloth.new(line).to_html}
+    when 'textile'
+      @data_for_output.collect! {|line| RedCloth.new(line).to_html}
+    when 'bbcode'
+      @data_for_output.collect! {|line| line.bbcode_to_html}
+    end
+  end
+
+  # Saves output to a file (one paragraph per line).
+  def save_to_file
+    File.open(@output, 'w+') do |file|
+      file.puts HEADER if @additional_html
+      file.puts @data_for_output.join("\n")
+      file.puts FOOTER if @additional_html
+    end
+  end
+end
+
+  # Header for an output file.
+  HEADER = <<-EOF
+<html>
+  <head>
+  <style type="text/css">
+      #wrapper {
+        width: 600px; margin: 0 auto;
+        font-family: "Trebuchet MS", Verdana, sans-serif;
+        border-top: 1px solid black;
+        border-bottom: 1px solid black;
+        line-height: 1.5em;
+      }
+      pre {
+        font-size:90%;
+        line-height:1.5em !important;
+        overflow:auto;
+        margin:1em 0;
+        padding:0.5em;
+      }
+      
+      pre, code {
+        font-family:Monaco, "Courier New", monospace;
+        color:#444;
+        background-color:#F8F8FF;
+        border:1px solid #DEDEDE;
+      }
+    </style>
+    <title>ClothMark file preview</title>
+  </head>
+  <body>
+    <div id="wrapper">
+  EOF
+
+  # Footer for an output file.
+  FOOTER = <<-EOF
+    </div>
+  </body>
+</html>
+  EOF
---
+++ spec/clothmark_spec.rb
@@ -0,0 +1,164 @@
+require File.expand_path(File.dirname(__FILE__) + '/spec_helper')
+
+describe ClothMark do
+  before :all do
+    @egg = ClothMark.new('example.txt', 'markdown', false)
+
+    # Create input text for BBCode
+    @bbcode_file = 'bbcode.txt'
+    BBCODE = "[b]O HAI![/b]
+      
+Lorem [i]ipsum[/i] dolor sit amet, [b]consectetur[/b] adipiscing elit.
+    
+[quote]Blaargh![/quote]" 
+
+    # Create input text for Markdown
+    @markdown_file = 'markdown.txt'
+    MARKDOWN = "# O HAI!
+    
+Lorem *ipsum* dolor sit amet, **consectetur** adipiscing elit."
+
+    # Create input text for Textile
+    @textile_file = 'textile.txt'
+    TEXTILE = "h1. O HAI!
+    
+Lorem _ipsum_ dolor sit amet, *consectetur* adipiscing elit."
+
+    # Save input text to files
+    [@bbcode_file, @markdown_file, @textile_file, 'example.txt'].each do |markup|
+      File.open(markup, 'w+') do |file|
+        case markup
+        when @bbcode_file
+          file.puts BBCODE
+        when @markdown_file
+          file.puts MARKDOWN
+        when @textile_file
+          file.puts TEXTILE
+        else
+          file.puts "OH MY!"
+        end
+      end
+    end
+
+    @bbmark = ClothMark.new(@bbcode_file, 'bbcode')
+    @bluemark = ClothMark.new(@markdown_file, 'markdown')
+    @redmark = ClothMark.new(@textile_file, 'textile')
+
+    [@bbmark, @bluemark, @redmark].each do |clothmark|
+      clothmark.read_from_file
+      clothmark.to_html
+    end
+  end
+
+  before :each do
+    @foo = ClothMark.new('nothing_special.txt', 'markdown', true, nil)
+  end
+
+  it "should raise an error if unknown markup language has been specified" do
+    lambda { ClothMark.new('rotfl', 'wikitext') }.should raise_error(ArgumentError)
+  end
+
+  it "should generate valid default 'output' filename" do
+    @foo.output.should == 'nothing_special_clothmark.html'
+  end
+
+  it "should gets output filename if it is specified" do
+    @bar = ClothMark.new('rails.txt', 'markdown', true, 'rubyonrails.txt')
+    @bar.output.should == 'rubyonrails.txt'
+  end
+  
+  it "should save valid data (one paragraph per line)" do
+  @foo.data_for_output = ['Donec ultrices tortor non lorem egestas ut pharetra diam vestibulum.',
+    'Etiam magna urna, porta non scelerisque ut, porttitor non purus.',
+    'Mauris blandit dui ac eros varius quis lacinia velit semper.']
+    
+    @foo.save_to_file
+    
+    counter = 0
+    
+    File.open(@foo.output, 'r') do |file|
+      # There's more than one paragraph in 'input file' (data_for_output),
+      # so we wan't a few paragraphs also in 'output' file.
+      file.each_line do |line|
+        counter += 1 unless line.empty?
+      end
+    end
+    
+    # File should have at least two paragraphs
+    counter.should > 1
+  end
+
+  it "should save data for output to the output file with additional CSS and HTML" do
+    @foo.data_for_output = ["OH MY!"]
+    @foo.save_to_file
+    
+    test_string = ''
+    
+    # Puts each file from output to string for test
+    File.open(@foo.output, 'r') do |file|
+    file.each_line do |line|
+        test_string << "#{line} \n"
+      end
+    end
+       
+    # Additional HTML and CSS should be added
+    test_string.should match(/<html/)
+    test_string.should match(/<head/)
+    test_string.should match(/<style/)
+    test_string.should match(/<body/)
+  end
+
+   it "should save data for output to the output file without additional HTML and CSS" do
+
+
+    @egg.read_from_file
+    @egg.save_to_file
+    
+    test_string = ''
+    
+    # Puts each file from output to string for test
+    File.open(@egg.output, 'r') do |file|
+      file.each_line do |line|
+        test_string << "#{line} \n"
+      end
+    end
+    
+    # Additional HTML and CSS should not be added
+    test_string.should_not match(/<html/)
+    test_string.should_not match(/<head/)
+    test_string.should_not match(/<style/)
+    test_string.should_not match(/<body/)
+  end 
+
+  it "should convert data from input file to HTML (BBCode -> HTML)" do
+    @bbmark.data_for_output.should_not be_empty
+    @bbmark.data_for_output.join("\n").should match(/<.+>/) # <.+> is the HTML tag
+  end
+
+  it "should convert data from input file to HTML (Markdown -> HTML)" do
+    @bluemark.data_for_output.should_not be_empty
+    @bluemark.data_for_output.join("\n").should match(/<.+>/)
+  end
+
+  it "should convert data from input file to HTML (Textile -> HTML)" do
+    @redmark.read_from_file
+    @redmark.to_html
+    @redmark.data_for_output.should_not be_empty
+    @redmark.data_for_output.join("\n").should match(/<.+>/)
+  end
+
+  after :all do
+    # Remove testfiles (outputs)
+    [@bbcode_file, @markdown_file, @textile_file].each do |file|
+      FileUtils.rm(file)
+    end
+    [@foo, @egg].each do |file|
+      begin
+        FileUtils.rm(file.output)
+        FileUtils.rm(file.file)
+      rescue Errno::ENOENT => e
+        # puts e
+      end
+    end
+  end
+end
